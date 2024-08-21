15451be023058d03fdfb970d3f6c3260a23bd917
Kur-Achan <achananyiethkur@gmail.com>
2024-08-21 22:52:21 +0300

    add files

--- schooll/settings.py
+++ schooll/settings.py
@@ -80,13 +80,9 @@ WSGI_APPLICATION = 'schooll.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "schooldatabase",
-        "USER": "school_head",
-        "PASSWORD": "mypassword",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
